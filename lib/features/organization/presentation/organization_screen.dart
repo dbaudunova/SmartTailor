09acabf1206cab89e7b9cf5e5ccbb1c4afd5a32d
Rodion Khripchenko <assassinrodiob@gmail.com>
2024-06-18 19:52:11 +0300

    fix

--- lib/features/organization/presentation/organization_screen.dart
+++ lib/features/organization/presentation/organization_screen.dart
@@ -12,64 +12,9 @@ class OrganizationScreen extends StatefulWidget {
   State<OrganizationScreen> createState() => _OrganizationScreenState();
 }
 
-int selectedSegment = 0;
-
-class _OrganizationScreenState extends State<OrganizationScreen> with RestorationMixin {
-  RestorableInt currentSegment = RestorableInt(0);
-
+class _OrganizationScreenState extends State<OrganizationScreen> {
   @override
   Widget build(BuildContext context) {
-    const segmentedControlMaxWidth = 450.0;
-    final children = <int, Widget>{
-      0: Text(t.order),
-      1: Text(t.equipment),
-      2: Text(t.service),
-    };
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        backgroundColor: AppColors.background,
-        automaticallyImplyLeading: false,
-        middle: SizedBox(
-          width: segmentedControlMaxWidth,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: CupertinoSlidingSegmentedControl<int>(
-              children: children,
-              onValueChanged: onValueChanged,
-              groupValue: currentSegment.value,
-            ),
-          ),
-        ),
-      ),
-      child: CupertinoNavigationBar(
-        backgroundColor: AppColors.background,
-        automaticallyImplyLeading: false,
-        middle: SizedBox(
-          width: segmentedControlMaxWidth,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: CupertinoSlidingSegmentedControl<int>(
-              children: children,
-              onValueChanged: onValueChanged,
-              groupValue: currentSegment.value,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  @override
-  String get restorationId => 'cupertino_segmented_control';
-
-  @override
-  void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
-    registerForRestoration(currentSegment, 'current_segment');
-  }
-
-  void onValueChanged(int? newValue) {
-    setState(() {
-      currentSegment.value = newValue!;
-    });
+    return Text('text');
   }
 }
