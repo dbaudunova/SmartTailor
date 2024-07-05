5b6da5f54d71b204e551cd6a63893fc7d930a883
Dilnaz27 <dbaudunova@gmail.com>
2024-07-05 17:20:30 +0600

    refactor: change marketplace card for avoiding bottom insets

--- lib/features/marketplace/presentation/pages/widgets/marketplace_card_widget.dart
+++ lib/features/marketplace/presentation/pages/widgets/marketplace_card_widget.dart
@@ -25,61 +25,60 @@ class MarketplaceCard extends StatelessWidget {
       child: Container(
         margin: const EdgeInsets.only(bottom: 16, left: 16, right: 16),
         padding: const EdgeInsets.only(bottom: 8, left: 8, right: 16, top: 8),
-        height: 87,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10),
           color: AppColors.white,
         ),
         child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 71,
+              width: 80,
+              margin: const EdgeInsets.only(right: 16),
+              decoration: const BoxDecoration(
+                color: AppColors.error,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
-                    height: 71,
-                    width: 80,
-                    margin: const EdgeInsets.only(
-                      right: 16,
+                  Text(
+                    title,
+                    style: AppTextStyle.textField16,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    description,
+                    style: AppTextStyle.textField16.copyWith(
+                      fontSize: 14,
+                      color: AppColors.greyText,
                     ),
-                    decoration: const BoxDecoration(
-                        color: AppColors.error,
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(10),
-                        )),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
                   ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        title,
-                        style: AppTextStyle.textField16,
+                  const SizedBox(height: 8),
+                  if (tabIndex == 0)
+                    Text(
+                      data?.toString() ?? '',
+                      style: AppTextStyle.textField16.copyWith(
+                        fontSize: 12,
                       ),
-                      const SizedBox(height: 8),
-                      Text(
-                        description,
-                        style: AppTextStyle.textField16.copyWith(
-                          fontSize: 14,
-                          color: AppColors.greyText,
-                        ),
-                      ),
-                      const SizedBox(height: 8),
-                      tabIndex == 0
-                          ? Text(
-                              data.toString(),
-                              style: AppTextStyle.textField16.copyWith(
-                                fontSize: 12,
-                              ),
-                            )
-                          : Container(),
-                    ],
-                  ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
                 ],
               ),
-              Flexible(child: _buildPriceText())
-            ]),
+            ),
+            const SizedBox(width: 8),
+            _buildPriceText(),
+          ],
+        ),
       ),
     );
   }
