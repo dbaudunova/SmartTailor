d542d3c5a08d2e21944ca795a98f539886022b32
Dilnaz27 <dbaudunova@gmail.com>
2024-07-28 15:45:26 +0600

    T1-187 feat: fetch data fot profile in personal data screen

--- lib/features/profile/presentation/pages/profile_screen.dart
+++ lib/features/profile/presentation/pages/profile_screen.dart
@@ -1,9 +1,17 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:neobis_smart_tailor/core/app/io_ui.dart';
+import 'package:neobis_smart_tailor/core/app/router/app_routes.dart';
+import 'package:neobis_smart_tailor/core/app/router/routes_path_const.dart';
+import 'package:neobis_smart_tailor/core/app/widgets/alert_dialog_style.dart';
+import 'package:neobis_smart_tailor/core/app/widgets/app_bar_style.dart';
+import 'package:neobis_smart_tailor/core/network/entity/state_status.dart';
 import 'package:neobis_smart_tailor/features/profile/presentation/bloc/profile_bloc.dart';
-import 'package:neobis_smart_tailor/features/profile/presentation/pages/profile_context.dart';
-import 'package:neobis_smart_tailor/injection/injection.dart';
+import 'package:neobis_smart_tailor/features/profile/presentation/widgets/exit_alert.dart';
+import 'package:neobis_smart_tailor/features/profile/presentation/widgets/profile_button_style.dart';
+import 'package:neobis_smart_tailor/features/profile/presentation/widgets/subscribe_container_style.dart';
+import 'package:neobis_smart_tailor/features/profile/presentation/widgets/user_info.dart';
 
 @RoutePage()
 class ProfileScreen extends StatefulWidget {
@@ -14,11 +22,154 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  bool _isHistoryOfOrdersButtonVisible = false;
+  bool _isSubscribeContainerVisible = true;
+
+  @override
+  void initState() {
+    BlocProvider.of<ProfileBloc>(context)
+        .add(const ProfileEvent.getProfileInfo());
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return BlocProvider.value(
-      value: getIt<ProfileBloc>(),
-      child: const ProfileContext(),
+    return Scaffold(
+      appBar: const AppBarStyle(
+        centerTitle: true,
+        title: 'Профиль',
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: AppProps.kPageMargin)
+            .copyWith(
+          top: AppProps.kSmallMargin,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildBlocBuilder(),
+            const SizedBox(height: AppProps.kTwentyMargin),
+            if (_isSubscribeContainerVisible)
+              SubscribeContainerStyle(
+                buttonTitle: 'Отправить запрос',
+                onButtonPressed: () {
+                  _buildShowDialog(context);
+                },
+              ),
+            const SizedBox(height: AppProps.kPageMargin),
+            _buildProfileButton(
+                context, 'Личные данные', const PersonalDataRoute()),
+            const SizedBox(height: AppProps.kPageMargin),
+            _buildProfileButton(
+                context, 'Мои объявления', const MyAnnouncementsRoute()),
+            const SizedBox(height: AppProps.kPageMargin),
+            _buildProfileButton(
+                context, 'Мои покупки', const MyPurchasesRoute()),
+            if (_isHistoryOfOrdersButtonVisible) ...[
+              const SizedBox(height: AppProps.kPageMargin),
+              _buildProfileButton(
+                  context, 'История заказов', const OrderHistoryRoute()),
+              const SizedBox(height: AppProps.kPageMargin),
+              _buildProfileButton(
+                  context, 'Организация', const ProfileOrganizationRoute()),
+            ],
+            const Spacer(),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: ElevatedButtonWidget(
+                text: 'Выйти из профиля',
+                onTap: () {
+                  _buildExitDialog(context);
+                },
+              ),
+            ),
+            const SizedBox(height: 16),
+          ],
+        ),
+      ),
+    );
+  }
+
+  BlocBuilder<ProfileBloc, ProfileState> _buildBlocBuilder() {
+    return BlocBuilder<ProfileBloc, ProfileState>(
+      builder: (context, state) {
+        if (state.stateStatus == const StateStatus.loading()) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        if (state.stateStatus ==
+            StateStatus.failure(message: '${state.stateStatus}')) {
+          return AppSnackBar.show(
+              context: context,
+              titleText: 'Не удалось загрузить данные',
+              error: true);
+        }
+        if (state.stateStatus == const StateStatus.success()) {
+          return UserInfo(
+            profileEntity: state.profile,
+            secondRowText: 'Подписка оформлена!',
+            thirdRowText: 'Срок до 1 августа 2024',
+            onIconPressed: () {
+              AutoRouter.of(context).push(const NotificationRoute());
+            },
+          );
+        }
+        return const Center(
+          child: Text('Что-то пошло не так'),
+        );
+      },
+    );
+  }
+
+  Widget _buildProfileButton(
+      BuildContext context, String title, PageRouteInfo route) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: ProfileButtonStyle(
+        title: title,
+        onPressed: () {
+          AutoRouter.of(context).push(route);
+        },
+      ),
+    );
+  }
+
+  void _buildExitDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return ExitAlert(
+          title: 'Вы действительно хотите выйти?',
+          confirmButton: () {
+            context.read<ProfileBloc>().add(const ProfileEvent.signOut());
+            AutoRouter.of(context).replaceNamed(RoutesPaths.enter);
+          },
+          cancelButton: () {
+            AutoRouter.of(context).maybePop();
+          },
+        );
+      },
+    );
+  }
+
+  void _buildShowDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: _alertDialogBuilder,
+    );
+  }
+
+  Widget _alertDialogBuilder(context) {
+    return AlertDialogStyle(
+      title: 'Ура!',
+      content: 'Подписка уже в пути!',
+      buttonText: 'Понятно',
+      onButtonPressed: () {
+        setState(() {
+          _isHistoryOfOrdersButtonVisible = true;
+          _isSubscribeContainerVisible = false;
+        });
+        AutoRouter.of(context).maybePop();
+      },
     );
   }
 }
